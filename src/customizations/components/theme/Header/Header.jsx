de095e758133e6576989d90d643d49054bca1f45
root <root@ip-172-31-13-33.eu-west-2.compute.internal>
2022-01-18 16:27:23 +0000

    Header component: Logo, Title, Description, Design structure

---
+++ src/customizations/components/theme/Header/Header.jsx
@@ -0,0 +1,74 @@
+/**
+ * Header component.
+ * @module components/theme/Header/Header
+ */
+
+import React, { Component } from 'react';
+import { Container, Segment } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { Logo, Title } from '@plone/volto/components';
+
+/**
+ * Header component class.
+ * @class Header
+ * @extends Component
+ */
+class Header extends Component {
+  /**
+   * Property types.
+   * @property {Object} propTypes Property types.
+   * @static
+   */
+  static propTypes = {
+    token: PropTypes.string,
+    pathname: PropTypes.string.isRequired,
+    content: PropTypes.shape({
+      title: PropTypes.string,
+      description: PropTypes.string,
+    }),
+  };
+
+  /**
+   * Default properties.
+   * @property {Object} defaultProps Default properties.
+   * @static
+   */
+  static defaultProps = {
+    token: null,
+    content: null,
+  };
+
+  /**
+   * Render method.
+   * @method render
+   * @returns {string} Markup for the component.
+   */
+  render() {
+    return (
+      <Segment basic className="header-wrapper" role="banner">
+        <Container>
+          <div className="header">
+            <div className="logo-nav-wrapper">
+              <div className="logo">
+                <Logo />
+              </div>
+            </div>
+            <div className="title-nav-wrapper">
+              <Title title={this.props.content?.title} />
+              <p className="documentDescription">
+                {this.props.content?.description}
+              </p>
+            </div>
+          </div>
+        </Container>
+      </Segment>
+    );
+  }
+}
+
+export default connect((state) => ({
+  token: state.userSession.token,
+  content: state.content.data,
+}))(Header);
